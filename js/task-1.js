28162cf1fbe07456f511d39c7f1a7a1c0a55dbe1
Anna Kutsay <annazadorozhna1990@gmail.com>
2026-01-29 20:07:06 +0200

    hm

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+const categories = document.querySelector("#categories");
+const categoriesItems = categories.querySelectorAll(".item");
+
+console.log("Number of categories:", categoriesItems.length);
+
+categoriesItems.forEach((item) => {
+  const title = item.querySelector("h2").textContent;
+  const elementsCount = item.querySelectorAll("li").length;
+
+  console.log("Category:", title);
+  console.log("Elements:", elementsCount);
+});
---
+++ js/task-4.js
@@ -0,0 +1,21 @@
+const form = document.querySelector(login - form);
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const emailValue = form.querySelector('input[name="email"]').value.trim();
+  const passwordValue = form
+    .querySelector('input[name="password"]')
+    .value.trim();
+
+  if (emailValue === "" || passwordValue === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  const formData = {
+    email: emailValue,
+    password: passwordValue,
+  };
+
+  console.log(formData);
+  form.reset();
+});
